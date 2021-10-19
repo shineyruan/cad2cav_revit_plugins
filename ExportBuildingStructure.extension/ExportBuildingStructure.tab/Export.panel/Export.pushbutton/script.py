8b215d2dc80650c191e3f9f0ae5f326ecc646d0e
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-10-19 00:36:41 -0400

    update coordinates from ft to meters

--- ExportBuildingStructure.extension/ExportBuildingStructure.tab/Export.panel/Export.pushbutton/script.py
+++ ExportBuildingStructure.extension/ExportBuildingStructure.tab/Export.panel/Export.pushbutton/script.py
@@ -1,5 +1,7 @@
 """Calculates total volume of all walls in the model."""
 
+FEET2METER = 0.3048
+
 from Autodesk.Revit import DB
 
 doc = __revit__.ActiveUIDocument.Document
@@ -7,7 +9,7 @@ doc = __revit__.ActiveUIDocument.Document
 # Creating collector instance and collecting all the walls from the model
 wall_collector = DB.FilteredElementCollector(doc).OfCategory(
     DB.BuiltInCategory.OST_Walls).WhereElementIsNotElementType()
-print("Total number of wall is: {}".format(len(wall_collector.ToElements())))\
+print("Total number of wall is: {}".format(len(wall_collector.ToElements())))
 
 window_collector = DB.FilteredElementCollector(doc).OfCategory(
     DB.BuiltInCategory.OST_Windows).WhereElementIsNotElementType()
@@ -24,19 +26,27 @@ for wall in wall_collector:
   if isinstance(loc_param, DB.LocationCurve):
     curve = loc_param.Curve
     if isinstance(curve, DB.Line):
-      print("wall,{},{},{},{},{},{}".format(
-          curve.GetEndPoint(0).X,
-          curve.GetEndPoint(0).Y,
-          curve.GetEndPoint(0).Z,
-          curve.GetEndPoint(1).X,
-          curve.GetEndPoint(1).Y,
-          curve.GetEndPoint(1).Z))
+      print("wall,{},{},{},{},{},{}".format(FEET2METER * curve.GetEndPoint(0).X,
+                                            FEET2METER * curve.GetEndPoint(0).Y,
+                                            FEET2METER * curve.GetEndPoint(0).Z,
+                                            FEET2METER * curve.GetEndPoint(1).X,
+                                            FEET2METER * curve.GetEndPoint(1).Y,
+                                            FEET2METER *
+                                            curve.GetEndPoint(1).Z))
       print("\n")
 
 # iterate door and collect door shape
 for door in door_collector:
   loc_param = door.Location
   if isinstance(loc_param, DB.LocationPoint):
-    print("door,{},{},{},0.0,0.0,0.0".format(loc_param.Point.X,
-                                             loc_param.Point.Y,
-                                             loc_param.Point.Z))
+    print("door,{},{},{},0.0,0.0,0.0".format(FEET2METER * loc_param.Point.X,
+                                             FEET2METER * loc_param.Point.Y,
+                                             FEET2METER * loc_param.Point.Z))
+
+# iterate window and collect window shape
+for window in window_collector:
+  loc_param = window.Location
+  if isinstance(loc_param, DB.LocationPoint):
+    print("window,{},{},{},0.0,0.0,0.0".format(FEET2METER * loc_param.Point.X,
+                                               FEET2METER * loc_param.Point.Y,
+                                               FEET2METER * loc_param.Point.Z))
